b31d831d1906e83a91d4058c51fdae0bda14f76f
jurajzalabai <juraj.zalabai@gmail.com>
2021-11-29 17:34:37 +0100

    fixing merge

--- src/gl9_scene/human.cpp
+++ src/gl9_scene/human.cpp
@@ -6,8 +6,8 @@
 #include "human.h"
 #include "spear.h"
 
-#include <shaders/diffuse_vert_glsl.h>
-#include <shaders/diffuse_frag_glsl.h>
+#include <shaders/scene_diffuse_vert_glsl.h>
+#include <shaders/scene_diffuse_frag_glsl.h>
 
 
 // Static resources
@@ -27,37 +27,41 @@ Human::Human() {
     position = keyframes[0].position;
     rotation = keyframes[0].rotation;
 
-    if (!shader) shader = std::make_unique<ppgso::Shader>(diffuse_vert_glsl, diffuse_frag_glsl);
+    if (!shader) shader = std::make_unique<ppgso::Shader>(scene_diffuse_vert_glsl, scene_diffuse_frag_glsl);
     if (!texture) texture = std::make_unique<ppgso::Texture>(ppgso::image::loadBMP("HumanTexture.bmp"));
     if (!mesh) mesh = std::make_unique<ppgso::Mesh>("Human.obj");
 }
 bool Human::update(Scene &scene, float dt) {
     age += dt;
 
-    if (keyframes[curr].startTime < age) {
-        if (keyframes[curr].duration != 0) {
-            if (age < keyframes[curr].startTime + keyframes[curr].duration){
-                position = lerp(keyframes[curr].position, keyframes[curr+1].position, age, keyframes[curr].startTime, keyframes[curr].duration);
-                rotation = lerp(keyframes[curr].rotation, keyframes[curr+1].rotation, age, keyframes[curr].startTime, keyframes[curr].duration);
+    if (scene.name){
+        if (keyframes[curr].startTime < age) {
+            if (keyframes[curr].duration != 0) {
+                if (age < keyframes[curr].startTime + keyframes[curr].duration){
+                    position = lerp(keyframes[curr].position, keyframes[curr+1].position, age, keyframes[curr].startTime, keyframes[curr].duration);
+                    rotation = lerp(keyframes[curr].rotation, keyframes[curr+1].rotation, age, keyframes[curr].startTime, keyframes[curr].duration);
+                }
+                else {
+                    curr++;
+                }
             }
-            else {
-                curr++;
+        }
+
+        if(curr == 1 && child == nullptr) {
+            for (auto &obj : scene.objects) {
+
+                if (obj.get() == this)
+                    continue;
+
+                auto spear = dynamic_cast<Spear *>(obj.get());
+                if (!spear) continue;
+
+                spear->parent = this;
+                child = spear;
             }
         }
     }
-
-    if(curr == 1 && child == nullptr) {
-        for (auto &obj : scene.objects) {
-
-            if (obj.get() == this)
-                continue;
-
-            auto spear = dynamic_cast<Spear *>(obj.get());
-            if (!spear) continue;
-
-            spear->parent = this;
-            child = spear;
-        }
+    else{
     }
 
     generateModelMatrix();
@@ -67,9 +71,30 @@ bool Human::update(Scene &scene, float dt) {
 
 void Human::render(Scene &scene) {
     shader->use();
+    shader->setUniform("pointLights[0].position", {0,15,74});
+    shader->setUniform("pointLights[0].constant", 1.0f);
+    shader->setUniform("pointLights[0].linear", 0.0f);
+    shader->setUniform("pointLights[0].quadratic", 0.0f);
+    shader->setUniform("pointLights[0].color", {1.0f, 1.0f, 1.0f});
+    shader->setUniform("pointLights[0].direction", {0.0f, -1.0f, 0.0f});
+    shader->setUniform("pointLights[0].outerCutOff", glm::cos(glm::radians(80.0f)));
+    shader->setUniform("pointLights[0].cutOff",  glm::cos(glm::radians(40.0f)));
 
-    // Set up light
-    shader->setUniform("LightDirection", scene.lightDirection);
+    shader->setUniform("pointLights[1].position", {0,1,35});
+    shader->setUniform("pointLights[1].constant", 1.0f);
+    shader->setUniform("pointLights[1].linear", 0.0f);
+    shader->setUniform("pointLights[1].quadratic", 0.0f);
+    shader->setUniform("pointLights[1].color", {1.0f, 0.5f, 0});
+    shader->setUniform("pointLights[1].direction", {0.5f, 0.5f, 0.5f});
+    shader->setUniform("pointLights[1].outerCutOff", glm::cos(glm::radians(180.0f)));
+    shader->setUniform("pointLights[1].cutOff",  glm::cos(glm::radians(180.0f)));
+
+    shader->setUniform("diffuse_strength", 0.3f);
+    shader->setUniform("ambient_strength", 0.2f);
+    shader->setUniform("specular_strength", 0.3f);
+    shader->setUniform("viewPos", scene.camera->position);
+
+
 
     // use camera
     shader->setUniform("ProjectionMatrix", scene.camera->projectionMatrix);
